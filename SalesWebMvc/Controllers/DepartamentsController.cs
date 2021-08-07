51b33e06814c043830306a12ec8df205da92648a
Waisman Braga <waismanfb@gmail.com>
2021-08-07 15:13:14 -0300

    First Model-Controller-View Departament

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> departaments = new List<Departament>();
+            departaments.Add(new Departament { Id = 1, Name = "Eletronics" });
+            departaments.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(departaments);
+        }
+    }
+}
--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,8 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
             ViewData["E-mail"] = "waismanfb@gmail.com";
+            ViewData["Student"] = "Waisman F Braga";
 
             return View();
         }
